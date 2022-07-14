80b6a2d00cebf0d970ee24bee2a75b1f9002769d
kikeestrada <kikeestrada@users.noreply.github.com>
2022-07-14 17:31:05 -0600

    add gsap

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,7 @@
 import {swDetecter} from './modules/swDetecter';
+import {gsapComponent} from './modules/gsapComponent';
+
 (()=>{
 	swDetecter();
-	if (document.body.classList.contains('home')) {
-		// functions here
-	}else if (document.body.classList.contains('page2')) {
-		// functions here
-	}else if (document.body.classList.contains('page3')) {
-		// functions here
-	}
+	gsapComponent();
 })();
